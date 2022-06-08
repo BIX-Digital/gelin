a114427a8cd3107986601da28e3557c162fd900f
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2022-06-08 10:39:51 +0200

    Made models accessible

--- lib/gelin.dart
+++ lib/gelin.dart
@@ -3,3 +3,4 @@ library gelin;
 export './src/generators/random_segment_generator.dart';
 export 'src/generators/defined_segment_generator.dart';
 export 'src/widgets/curves_potter.dart';
+export 'src/models/graphic_objects.dart';
