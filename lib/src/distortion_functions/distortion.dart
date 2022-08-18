ac431c383b0f649062cc0ac385ae8b570b01585b
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2022-08-18 11:56:59 +0200

    Slightly modified distortion value

--- lib/src/distortion_functions/distortion.dart
+++ lib/src/distortion_functions/distortion.dart
@@ -2,28 +2,28 @@ import 'dart:math';
 
 import '../models/graphic_objects.dart';
 
-
 // This is distortion function example
-AdjustableCurve2D simpleDistortionFunc(AdjustableCurve2D curve, {int pointsToDistort = 2}) {
+AdjustableCurve2D simpleDistortionFunc(AdjustableCurve2D curve,
+    {int pointsToDistort = 2}) {
   // Let's distort points in the line
   if (curve.coordinates.length < pointsToDistort) {
     throw Exception("Line length should be bigger then $pointsToDistort");
   }
-  var distortionStartPoint = Random().nextInt(curve.coordinates.length - pointsToDistort);
+  var distortionStartPoint =
+      Random().nextInt(curve.coordinates.length - pointsToDistort);
   for (var i = distortionStartPoint;
-  i < distortionStartPoint + pointsToDistort;
-  i++) {
+      i < distortionStartPoint + pointsToDistort;
+      i++) {
     // Use std to find axis to distort coordinates
     // if std X is bigger - distort Y and vice versa
     List coordStd = curve.getCoordinatesStd();
     if (coordStd[0] < coordStd[1]) {
-      var x = curve.coordinates[i].x + 10;
+      var x = curve.coordinates[i].x + 15;
       curve.coordinates[i] = Coord(x, curve.coordinates[i].y);
     } else {
-      var y = curve.coordinates[i].y + 10;
+      var y = curve.coordinates[i].y + 15;
       curve.coordinates[i] = Coord(curve.coordinates[i].x, y);
     }
-
   }
   return curve;
 }
